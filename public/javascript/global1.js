64dc00ce21c2c73699952992a3b06f23d0a7c2d4
wsavd <tuve@rootfest.net>
2017-04-13 11:50:45 +0300

    work columns

--- public/javascript/global1.js
+++ public/javascript/global1.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
-    console.log("global1 succes")
+    console.log("global1 succes");
+    $.ajax({
+            url: 'http://localhost:3001/board/58ee7bf57cb76c2e6477caca/columns',
+            type: 'GET',
+            contentType: 'application/json',
+            success: function(result) {
+                console.log(result);
+                $.each(result, function(index){
+                    $(".lists").append("<div class='column'><div class='title'>"+ result[index].title +"</div></div>");
+                })
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
 });
